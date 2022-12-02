f312eae2c9332e4b034d7fdb389a9f9de6e26b40
AngelFranciscoGarciaGuzman <111137354+AngelFranciscoGarciaGuzman@users.noreply.github.com>
2022-12-01 18:42:02 -0600

    Insertar menu en main
    Llamar a las funciones desde un main adecuado para el usuario

--- guesas.cpp
+++ guesas.cpp
@@ -5,13 +5,341 @@
 #include"papas.h"
 #include"orden.h"
 
+
+std::string none;
+
 int main(){
+
+
+    //Crear Objeto orden num_1
     Orden num_1;
-    num_1.agregaHamburguesa("Sin queso", "Con verduras", "BBQ", 0);
-    num_1.asignar_precio_h(80.5, 0);
-    num_1.agregaMalteada("Chocolate", 0.5, 0);
-    num_1.asignar_precio_m(50.0, 0);
-    num_1.agregaPapas("Clasicas", "Medianas", 0);
-    num_1.asignar_precio_p(35.5, 0);
-    std::cout << num_1.estatus_orden();
+
+
+    
+    std::cout << "La Hamburguesa 1 llevara queso?" << std::endl;
+
+    std::string queso_1;
+
+    getline(std::cin, queso_1);
+
+    std::cout << "Llevara verdura?" << std::endl;
+
+    std::string verdura_1;
+
+    getline(std::cin, verdura_1);
+
+    std::cout << "Tipo de salsa?" << std::endl;
+
+    std::string salsa_1;
+
+    getline(std::cin, salsa_1);
+
+    float precioh_1;
+
+    std::cout << "Asignar precio a hamburguesa" << std::endl;
+
+    std::cin >> precioh_1;
+
+    getline(std::cin, none);
+
+
+
+    
+    std::cout << "La Hamburguesa 2 llevara queso?" << std::endl;
+
+    std::string queso_2;
+
+    getline(std::cin, queso_2);
+
+    std::cout << "Llevara verdura?" << std::endl;
+
+    std::string verdura_2;
+
+    getline(std::cin, verdura_2);
+
+    std::cout << "Tipo de salsa?" << std::endl;
+
+    std::string salsa_2;
+
+    getline(std::cin, salsa_2);
+
+    float precioh_2;
+
+    std::cout << "Asignar precio a hamburguesa" << std::endl;
+
+    std::cin >> precioh_2;
+
+    getline(std::cin, none);
+
+
+
+    std::cout << "La Hamburguesa 3 llevara queso?" << std::endl;
+
+    std::string queso_3;
+
+    getline(std::cin, queso_3);
+
+    std::cout << "Llevara verdura?" << std::endl;
+
+    std::string verdura_3;
+
+    getline(std::cin, verdura_3);
+
+    std::cout << "Tipo de salsa?" << std::endl;
+
+    std::string salsa_3;
+
+    getline(std::cin, salsa_3);
+
+    float precioh_3;
+
+    std::cout << "Asignar precio a hamburguesa" << std::endl;
+
+    std::cin >> precioh_3;
+
+    getline(std::cin, none);
+
+
+
+    std::cout << "La Hamburguesa 4 llevara queso?" << std::endl;
+
+    std::string queso_4;
+
+    getline(std::cin, queso_4);
+
+    std::cout << "Llevara verdura?" << std::endl;
+
+    std::string verdura_4;
+
+    getline(std::cin, verdura_4);
+
+    std::cout << "Tipo de salsa?" << std::endl;
+
+    std::string salsa_4;
+
+    getline(std::cin, salsa_4);
+
+    float precioh_4;
+
+    std::cout << "Asignar precio a hamburguesa" << std::endl;
+
+    std::cin >> precioh_4;
+
+    getline(std::cin, none);
+
+
+
+
+    std::cout << "La Malteada 1, cual es su tamano?" << std::endl;
+
+    float malteadat_1;
+
+    std::cin >> malteadat_1;
+    getline(std::cin, none);
+
+    std::cout << "Sabor?" << std::endl;
+
+    std::string malteadas_1;
+
+    getline(std::cin, malteadas_1);
+
+    float preciom_1;
+
+    std::cout << "Asignar precio a la malteada" << std::endl;
+
+    std::cin >> preciom_1;
+    getline(std::cin, none);
+
+
+    std::cout << "La Malteada 2, cual es su tamano?" << std::endl;
+
+    float malteadat_2;
+
+    std::cin >> malteadat_2;
+    getline(std::cin, none);
+
+    std::cout << "Sabor?" << std::endl;
+
+    std::string malteadas_2;
+
+    getline(std::cin, malteadas_2);
+
+    float preciom_2;
+
+    std::cout << "Asignar precio a la malteada" << std::endl;
+
+    std::cin >> preciom_2;
+    getline(std::cin, none);
+
+
+    std::cout << "La Malteada 3, cual es su tamano?" << std::endl;
+
+    float malteadat_3;
+
+    std::cin >> malteadat_3;
+    getline(std::cin, none);
+
+    std::cout << "Sabor?" << std::endl;
+
+    std::string malteadas_3;
+
+    getline(std::cin, malteadas_3);
+
+    float preciom_3;
+
+    std::cout << "Asignar precio a la malteada" << std::endl;
+
+    std::cin >> preciom_3;
+    getline(std::cin, none);
+
+
+    std::cout << "La Malteada 4, cual es su tamano?" << std::endl;
+
+    float malteadat_4;
+
+    std::cin >> malteadat_4;
+    getline(std::cin, none);
+
+    std::cout << "Sabor?" << std::endl;
+
+    std::string malteadas_4;
+
+    getline(std::cin, malteadas_4);
+
+    float preciom_4;
+
+    std::cout << "Asignar precio a la malteada" << std::endl;
+
+    std::cin >> preciom_4;
+    getline(std::cin, none);
+
+
+
+    std::cout << "La Orden de papas 1, cual es el estilo?" << std::endl;
+
+    std::string estilo_1;
+
+    getline(std::cin, estilo_1);
+
+    std::cout << "tamano?" << std::endl;
+
+    std::string tamanop_1;
+
+    getline(std::cin, tamanop_1);
+
+    float preciop_1;
+
+    std::cout << "Asignar precio a orden de papas" << std::endl;
+
+    std::cin >> preciop_1;
+
+    getline(std::cin, none);
+
+
+    std::cout << "La Orden de papas 2, cual es el estilo?" << std::endl;
+
+    std::string estilo_2;
+
+    getline(std::cin, estilo_2);
+
+    std::cout << "tamano?" << std::endl;
+
+    std::string tamanop_2;
+
+    getline(std::cin, tamanop_2);
+
+    float preciop_2;
+
+    std::cout << "Asignar precio a orden de papas" << std::endl;
+
+    std::cin >> preciop_2;
+
+    getline(std::cin, none);
+
+
+    std::cout << "La Orden de papas 3, cual es el estilo?" << std::endl;
+
+    std::string estilo_3;
+
+    getline(std::cin, estilo_3);
+
+    std::cout << "tamano?" << std::endl;
+
+    std::string tamanop_3;
+
+    getline(std::cin, tamanop_3);
+
+    float preciop_3;
+
+    std::cout << "Asignar precio a orden de papas" << std::endl;
+
+    std::cin >> preciop_3;
+
+    getline(std::cin, none);
+
+
+    std::cout << "La Orden de papas 4, cual es el estilo?" << std::endl;
+
+    std::string estilo_4;
+
+    getline(std::cin, estilo_4);
+
+    std::cout << "tamano?" << std::endl;
+
+    std::string tamanop_4;
+
+    getline(std::cin, tamanop_4);
+
+    float preciop_4;
+
+    std::cout << "Asignar precio a orden de papas" << std::endl;
+
+    std::cin >> preciop_4;
+
+    getline(std::cin, none);
+
+
+    num_1.agregaHamburguesa(queso_1, verdura_1, salsa_1, 0);
+    num_1.asignar_precio_h(precioh_1, 0);
+
+    num_1.agregaHamburguesa(queso_2, verdura_2, salsa_2, 1);
+    num_1.asignar_precio_h(precioh_2, 1);
+
+    num_1.agregaHamburguesa(queso_3, verdura_3, salsa_3, 2);
+    num_1.asignar_precio_h(precioh_3, 2);
+
+    num_1.agregaHamburguesa(queso_4, verdura_4, salsa_4, 3);
+    num_1.asignar_precio_h(precioh_4, 3);
+
+
+    num_1.agregaMalteada(malteadas_1, malteadat_1, 0);
+    num_1.asignar_precio_m(preciom_1, 0);
+
+    num_1.agregaMalteada(malteadas_2, malteadat_2, 1);
+    num_1.asignar_precio_m(preciom_2, 1);
+
+    num_1.agregaMalteada(malteadas_3, malteadat_3, 2);
+    num_1.asignar_precio_m(preciom_3, 2);
+
+    num_1.agregaMalteada(malteadas_4, malteadat_4, 3);
+    num_1.asignar_precio_m(preciom_4, 3);
+
+
+
+    num_1.agregaPapas(estilo_1,tamanop_1, 0);
+    num_1.asignar_precio_p(preciop_1, 0);
+
+    num_1.agregaPapas(estilo_2,tamanop_2, 1);
+    num_1.asignar_precio_p(preciop_2, 1);
+
+    num_1.agregaPapas(estilo_3, tamanop_3, 2);
+    num_1.asignar_precio_p(preciop_3, 2);
+
+    num_1.agregaPapas(estilo_3,tamanop_3, 3);
+    num_1.asignar_precio_p(preciop_3, 3);
+
+    std::cout << num_1.estatus_orden() << std::endl;
+
+    system("pause");
 }
+
